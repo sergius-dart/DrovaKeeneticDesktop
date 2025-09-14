59e19bac5464396332198078cf0a0108b8c28b0b
Sergius-Dart <sergius-dart@yandex.ru>
2025-09-14 17:30:51 +0300

    Fix load env

--- drova_desktop_keenetic/common/__init__.py
+++ drova_desktop_keenetic/common/__init__.py
@@ -4,7 +4,7 @@ from pathlib import Path
 from dotenv import load_dotenv
 
 ENV_LOCATION = (
-    os.environ["ENV_LOCATION"] if hasattr(os.environ, "ENV_LOCATION") else Path(__file__).parent.parent.parent / ".env"
+    os.environ["ENV_LOCATION"] if "ENV_LOCATION" in os.environ else Path(__file__).parent.parent.parent / ".env"
 )
 
 load_dotenv(ENV_LOCATION)
