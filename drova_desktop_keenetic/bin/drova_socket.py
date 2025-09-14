c5aa7a40b190f065b50cc07f95d991bacedfdf28
Sergius-Dart <sergius-dart@yandex.ru>
2025-09-14 19:09:15 +0300

    minor fixes in sftp and logging

--- drova_desktop_keenetic/bin/drova_socket.py
+++ drova_desktop_keenetic/bin/drova_socket.py
@@ -1,9 +1,20 @@
 import asyncio
-from logging import DEBUG, basicConfig
+import os
+from logging import DEBUG, StreamHandler, basicConfig
+from logging.handlers import RotatingFileHandler
 
+from drova_desktop_keenetic.common.contants import DROVA_SOCKET_LISTEN
 from drova_desktop_keenetic.common.drova_socket import DrovaSocket
 
-basicConfig(level=DEBUG)
+assert DROVA_SOCKET_LISTEN in os.environ, "Need socket listening"
+
+ch = StreamHandler()
+handler_rotating = RotatingFileHandler(
+    f"app.{os.environ[DROVA_SOCKET_LISTEN]}.log", maxBytes=1024 * 1024, backupCount=5
+)
+
+
+basicConfig(level=DEBUG, handlers=(handler_rotating, ch))
 
 
 def run_async_main():
--- drova_desktop_keenetic/common/patch.py
+++ drova_desktop_keenetic/common/patch.py
@@ -20,7 +20,7 @@ class IPatch(ABC):
 
     async def patch(self):
         async with NamedTemporaryFile("ab") as temp_file:
-            f = await self.sftp.get(str(self.remote_file_location), temp_file.name):
+            f = await self.sftp.get(str(self.remote_file_location), temp_file.name)
             self._patch(f, Path(temp_file.name))
             await self.sftp.put(temp_file.name, str(self.remote_file_location))
 
