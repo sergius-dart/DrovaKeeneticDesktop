45600c33e73debf25dc61e4d1a758a06829d6d30
Sergius-Dart <sergius-dart@yandex.ru>
2026-05-01 23:07:20 +0300

    maybe fix obs - is started but no capture screen

--- drova_desktop_keenetic/patches/basic.py
+++ drova_desktop_keenetic/patches/basic.py
@@ -205,7 +205,7 @@ class Grypholink(ISessionHandler):
         assert ctx.sftp
         if self.TASKKILL_IMAGE:
             await ctx.ssh.run(str(TaskKill(image=self.TASKKILL_IMAGE)))
-            await asyncio.sleep(0.5)  # wait exit launcher
+            await asyncio.sleep(0.1)  # wait exit launcher
         await _clear_directory(ctx.sftp, self.remote_dir_clear)
 
     async def on_session_active(self, ctx: SessionHandlerContext):
